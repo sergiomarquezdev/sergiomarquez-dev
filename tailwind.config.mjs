5b0d68c323033f7b56a94e28cb3e97ddf6c0ccb4
Sergio Márquez <marquezpsergio@gmail.com>
2025-07-09 02:02:16 +0200

    feat: Align content with new personal brand

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'background': 'var(--background)',
+				'primary-text': 'var(--primary-text)',
+				'secondary-text': 'var(--secondary-text)',
+				'border-color': 'var(--border-color)',
+			},
+			fontFamily: {
+				'sans': 'var(--font-family-sans)',
+			}
+		},
 	},
 	plugins: [],
-}
+};
